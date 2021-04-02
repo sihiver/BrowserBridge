8bd87c92c96461ded4dc7153452d22fd7588ab1e
Shelikhoo <xiaokangwang@outlook.com>
2021-04-02 11:55:40 +0100

    move name to v2fly

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -3,7 +3,7 @@ package bridge
 import (
 	"fmt"
 	"github.com/gopherjs/websocket"
-	"github.com/xiaokangwang/BrowserBridge/proto"
+	"github.com/v2fly/BrowserBridge/proto"
 	"github.com/xtaci/smux"
 	"io"
 	"time"
--- bridge/bridgejs/main.go
+++ bridge/bridgejs/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gopherjs/gopherjs/js"
-	"github.com/xiaokangwang/BrowserBridge/bridge"
+	"github.com/v2fly/BrowserBridge/bridge"
 	"log"
 	"net/url"
 )
